892d631b8b7f33591c91e73b57636e51797538d3
harshit <harshit.sangani@openxcell.com>
2024-05-29 13:53:31 +0530

    initial commit

---
+++ src/hocs/with-auth-guard.js
@@ -0,0 +1,9 @@
+import { AuthGuard } from 'src/guards/auth-guard';
+
+// eslint-disable-next-line react/display-name
+export const withAuthGuard = Component => props =>
+  (
+    <AuthGuard>
+      <Component {...props} />
+    </AuthGuard>
+  );
